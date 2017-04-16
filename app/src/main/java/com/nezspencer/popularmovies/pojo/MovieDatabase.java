d66c913c7dd49c3360228946b3aebd509aa463ad
NezSpencer <nuhiara@gmail.com>
2017-04-16 20:37:57 +0100

    made changes and corrections in line with udacity reviews

--- app/src/main/java/com/nezspencer/popularmovies/pojo/MovieDatabase.java
+++ app/src/main/java/com/nezspencer/popularmovies/pojo/MovieDatabase.java
@@ -25,37 +25,12 @@ public class MovieDatabase implements Parcelable {
     private MovieDatabaseResults[] results;
     private int total_results;
 
-    public int getPage() {
-        return this.page;
-    }
 
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getTotal_pages() {
-        return this.total_pages;
-    }
-
-    public void setTotal_pages(int total_pages) {
-        this.total_pages = total_pages;
-    }
 
     public MovieDatabaseResults[] getResults() {
         return this.results;
     }
 
-    public void setResults(MovieDatabaseResults[] results) {
-        this.results = results;
-    }
-
-    public int getTotal_results() {
-        return this.total_results;
-    }
-
-    public void setTotal_results(int total_results) {
-        this.total_results = total_results;
-    }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
